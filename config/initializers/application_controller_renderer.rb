3e6cbbe6e2d6c52092c5a1775f56d1b8be465bd4
EC2 Default User <ec2-user@ip-172-31-69-246.ec2.internal>
2021-02-16 07:46:24 +0000

    Rubocop-airbnb

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '0b911fc4df3f748552d7dbbfe16ccc2698099a86f3d08f0e3d6cd5705fc82c4ec80edf2b95eb2bd2d2ea6a269dac211599c5d046f463d97892a643d66b5ef347'
